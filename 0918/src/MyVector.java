bd00348f05ff679f1b63fd55dfde6db6ef56e914
Chunggeol You <chunggryu@gmail.com>
2020-12-23 09:20:51 +0900

    MyVector Quiz is finished.

--- 0918/src/MyVector.java
+++ 0918/src/MyVector.java
@@ -310,7 +310,7 @@ public class MyVector {
 			//	results.append(data[i]);
 			//}
 			// Q4. ans.
-			if (i == 0) results.append(",");
+			if (i != 0) results.append(",");
 			results.append(get(i).toString());
 		}
 		
@@ -330,16 +330,24 @@ public class MyVector {
 	private class Itr implements Iterator {
 		int cursor = 0; // 읽어올 요소의 위치(index)
 		int lastRet = -1;
+		
 		public Object next() {
 			/*
 			 *  다음의 코드를 완성하시오.
 			 *  1. cursor가 가리키고 있는 위치(index)의 객체를 꺼내온다.(get() 사용)
 			 *  2. cursor의 값을 lastRet에 저장하고 cursor의 값을 1 증가시킨다.
 			 *  (에를 들어, cursor의 값이 1 이었으면 lastRet의 값은 1이 되고, cursor의 값은 2가 된다.)
-			 *  3. 1에서 꺼내온 객체를 반환한다
-			 *  
+			 *  3. 1에서 꺼내온 객체를 반환한다  
 			 */
-			return null;
+			
+			// 1. cursor가 가리키고 있는 위치(index)의 객체를 꺼내온다.(get() 사용)
+			Object result = get(cursor);
+			
+			// 2. cursor의 값을 lastRet에 저장하고 cursor의 값을 1 증가시킨다.
+			lastRet = cursor++;
+			
+			// 3. 1에서 꺼내온 객체를 반환한다
+			return result;
 		}
 		
 		public boolean hasNext() {
@@ -348,7 +356,7 @@ public class MyVector {
 			 *  hint : cursor의 값이 객체배열의 마지막요소의 위치(index)에 다다랐는지 확인한다.
 			 */
 			
-			return false;
+			return cursor < size;
 		}
 		
 		public void remove() {
@@ -361,8 +369,21 @@ public class MyVector {
 			 *  (현재 cursor의 위치보다 이전의 값이 삭제되면 cursor의 위치도 변경되어야 하므로)
 			 *  4. lastRet에 -1을 저장한다.(직전에 읽어온 객체가 삭제 되었으므로)
 			 */
+			// 1. lastRet의 값이 -1이면(직전에 읽어온 객체가 없거나 삭제 되었으면) IllegalStateException을 발생시킨다.
+			if (lastRet == -1) throw new IllegalStateException("유효하지 않은 lastRet 값입니다:"+lastRet);
+			
+			// 2. 직전에 읽어온 객체를 객체배열에서 제거한다.(MyVector의 remove() 사용)
+			//new MyVector().remove(lastRet);
+			MyVector.this.remove(lastRet);
+			
+			// 3. lastRet의 값이 cursor의 값보다 작으면 cursor의 값을 1 감소 시킨다.
+			if (lastRet < cursor) cursor--;
+			
+			// 4. lastRet에 -1을 저장한다.(직전에 읽어온 객체가 삭제 되었으므로)
+			lastRet = -1;
 		}
 		
 	} // private class Itr
+
 	
 } // class MyVector
--- 0918/src/MyVectorEx5.java
+++ 0918/src/MyVectorEx5.java
@@ -1,3 +1,16 @@
+/*
+ * [문제5] MyVector클래스에 iterator()를 완성하세요. 그리고 이 메서드를 구현하는데 필요한, Iterator인터페이스를 구현한 내부 클래스를 완성하세요.
+ * Iterator iterator() - MyVector클래스의 iterator를 반환한다.(Iterator인터페이스를 구현한 클래스의 인스턴스를 반환)
+ * 
+ * << Iterator인터페이스에 정의된 메서드 >>
+ * boolean hasNext() - 다음에 읽을 요소가 있는지 확인한다. 있으면 true, 없으면 false
+ * Object next() -  다음 요소를 읽어서 반환한다.
+ * void remove() -  읽어온 요소를 제거한다.(반드시 next()를 호출한 다음에 호출해야한다.
+ * [참고] iterator를 통해 MyVector의 요소들에 접근하는 동안, MyVector의 요소가 추가또는 삭제 되면 iterator는 예외를
+ * 발생시켜야한다. 여기서 그 것(fast-fail)에 대한 구현은 생략했다.
+ * Vector클래스에는 구현되어 있으니, 필요하다면 Vector클래스의 소스를 참고하도록 하자.
+ * [출처] [Java1000제] 객체배열 MyVector 구현하기 5 - iterator() (남궁성의 코드초보스터디(자바 java, c언어, javascript, python) | 작성자 남궁성
+ */
 import java.util.Iterator;
 
 public class MyVectorEx5 {
@@ -10,6 +23,8 @@ public class MyVectorEx5 {
 		v.add("CCC");
 		v.add("DDD");
 		
+		System.out.println("Before: "+v);
+		
 		Iterator it = v.iterator();
 		
 		while (it.hasNext()) {
@@ -21,6 +36,6 @@ public class MyVectorEx5 {
 		}
 		
 		System.out.println(v);
-	}
+	} // main
 
 }
