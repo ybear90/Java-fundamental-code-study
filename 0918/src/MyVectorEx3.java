c085768974c00c3fac70c4cb71246f3690725969
Chunggeol You <chunggryu@gmail.com>
2020-12-23 09:29:37 +0900

    MyVector Quiz is finished(include modify)

--- 0918/src/MyVectorEx3.java
+++ 0918/src/MyVectorEx3.java
@@ -1,10 +1,14 @@
 /*
- * [문제2] 다음의 MyVector클래스의 메서드들을 완성하세요.
-	void ensureCapacity(int minCapacity) - 객체배열이 최소한 지정된 용량을 확보하도록 한다.
-	void setCapacity(int newCapacity)   - 객체배열의 용량을 지정된 용량(newCapacity)으로 변경한다.
-	boolean add(Object obj) - 객체배열에 객체(obj)를 저장한다.
-	Object get(int index) - 객체배열에 저장된, index번째의 객체를 반환한다.
-	Object set(int index, Object obj) - 객체배열의 index번째의 위치에 객체(obj)를 저장하고, 기존의 객체를 반환한다.
+ * [문제3] 다음의 MyVector클래스의 메서드들을 완성하세요.
+ * boolean contains(Object obj) - 지정된 객체(obj)가 객체배열에 존재하는지 확인한다. 있으면 true, 없으면 false
+ * int indexOf(Object obj) - 지정된 객체의 위치(index)를 찾아서 반환한다. 객체배열에 없으면 -1을 반환
+ * (객체배열의 첫번째 요소부터 찾기 시작한다.)
+ * int lastIndexOf(Object obj) - 지정된 객체의 위치(index)를 찾아서 반환한다. 객체배열에 없으면 -1을 반환
+ * (객체배열의 마지막 요소부터 역순으로 찾기 시작한다.)
+ * int indexOf(Object obj, int index) - 지정된 객체를 지정한 위치(index)부터 찾기 시작한다. 객체배열에 없으면 -1을 반환
+ * int lastIndexOf(Object obj, int index)  - 지정된 객체를 지정한 위치(index)부터 찾기 시작한다. 객체배열에 없으면 -1을 반환
+ * (역순으로 찾기 시작한다.)
+ * [출처] [Java1000제] 객체배열 MyVector 구현하기 3 - indexOf(), contains() 등 (남궁성의 코드초보스터디(자바 java, c언어, javascript, python) | 작성자 남궁성
 */
 public class MyVectorEx3 {
 	
--- 0918/src/MyVectorEx4.java
+++ 0918/src/MyVectorEx4.java
@@ -1,4 +1,13 @@
-
+/*
+ * [문제4] 다음의 MyVector클래스의 메서드들을 완성하세요.
+ * void add(int index, Object obj) - 객체배열의 지정된 위치(index)에 객체(obj)를 추가한다.
+ * Object remove(int index) - 객체배열에서 index번째(index는 0부터 시작)에 있는 객체를 삭제한다.
+ * boolean remove(Object obj) - 객체배열에서 지정된 객체(obj)를 찾아서 삭제한다. 삭제에 성공하면 true, 실패하면 false
+ * void clear() - 객체배열을 비운다.
+ * Object[] toArray() -  객체배열을 복사해서 반환한다.
+ * String toString() - 객체배열에 저장된 모든 객체를 출력한다.(모든 객체의 toString()을 호출한다.)
+ * [출처] [Java1000제] 객체배열 MyVector 구현하기 4 - add(), remove(), toArray() 등 (남궁성의 코드초보스터디(자바 java, c언어, javascript, python) | 작성자 남궁성
+ */
 public class MyVectorEx4 {
 
 	public static void main(String[] args) {
@@ -29,6 +38,7 @@ public class MyVectorEx4 {
 		System.out.println("size:"+v.size());
 		System.out.println("capacity:"+v.capacity());
 		System.out.println("isEmpty:"+v.isEmpty());
+		System.out.println();
 		
 		v.clear();
 		System.out.println(v);
